34381332faa0b94cba084846c4fd3814e2ed194b
M1RAK <abdullahiismail1105@gmail.com>
2026-01-31 12:30:03 +0100

    updated payment methods

--- src/app/subscription/page.tsx
+++ src/app/subscription/page.tsx
@@ -84,7 +84,7 @@ export default function SubscriptionPage() {
 		tx_ref: Date.now().toString(),
 		amount: plan.amount,
 		currency: plan.currency || 'NGN',
-		payment_options: 'card, account, googlepay, applepay',
+		payment_options: 'card, banktransfer, account, ussd, opay',
 		payment_plan: plan.id, // Flutterwave plan ID
 		customer: {
 			email: user?.email || '',
